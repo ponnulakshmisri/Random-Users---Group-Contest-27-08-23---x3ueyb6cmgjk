354ea7e61c8d61e09d5a1b71a655d4b27a7a7d64
ponnulakshmisri <117030120+ponnulakshmisri@users.noreply.github.com>
2023-08-27 14:06:18 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,32 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Users from "./users";
 
-const App = () => {
-    const [items, setItems] = useState([]);
-    const [isLoading, setLoading] = useState(true);
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [userNames, setUserNames] = useState([]);
 
-    useEffect(() => {
-        fetch('https://randomuser.me/api/?results=10')
-        .then((response)=>response.json())
-        .then((data)=>{
-            const firstname=data.results.map((user)=>user.name.first);
-            setItems(firstname);
-            setTimeout(()=>{
-                setLoading(false);
-            },3000);
-        });
-    }, []);
+  useEffect(() => {
+   
+    fetch('https://randomuser.me/api/?results=10')
+      .then((response) => response.json())
+      .then((data) => {
+       
+        const firstNames = data.results.map((user) => user.name.first);
+        setUserNames(firstNames);
 
-    return <Users isLoading={isLoading} items={items} />;
-};
+      
+        setTimeout(() => {
+          setLoading(false);
+        }, 3000);
+      });
+  }, []);
+
+  return (
+    <div>
+     
+      {loading ? <div id="loading">Loading....</div> : <Users userNames={userNames} />}
+    </div>
+  );
+}
 
 export default App;
