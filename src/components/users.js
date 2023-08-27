3bbf5028b929105871e55c89a50187ae5bce887c
ponnulakshmisri <117030120+ponnulakshmisri@users.noreply.github.com>
2023-08-27 14:17:18 +0100

    Update users.js

--- src/components/users.js
+++ src/components/users.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Users({ isLoading, items }) {
-    return <div></div>;
+    console.clear();
+
+
+    return (
+        <div className="Users">
+        {!isLoading ?
+            (<div id="loading">
+                "Loading...."
+            </div>)        
+        :(
+            items.map((user, index)=>(
+                <div key={index} className="user">
+                    {user.name.first}
+                </div>
+            ))
+        )}
+        </div>
+        );
 }
 
 export default Users;
